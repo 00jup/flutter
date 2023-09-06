d0d2e88372ab4ea80aa291f87b479f68af3089ae
00jup <jupark0369@gmail.com>
2023-09-06 14:39:29 +0900

    PageView animateToPage

--- instagram/lib/main.dart
+++ instagram/lib/main.dart
@@ -3,17 +3,21 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        bottomAppBarTheme: BottomAppBarTheme(color: Colors.black),
-        iconTheme: IconThemeData(color: Colors.black),
-        appBarTheme: AppBarTheme(color: Colors.white, actionsIconTheme: IconThemeData(color: Colors.black, size: 50)),
-        textTheme: TextTheme(
-          bodyText2: TextStyle(color: Colors.red),
-        ),
+    theme: ThemeData(
+      bottomAppBarTheme: BottomAppBarTheme(color: Colors.black),
+      iconTheme: IconThemeData(color: Colors.black),
+      appBarTheme:
+      AppBarTheme(color: Colors.white, actionsIconTheme: IconThemeData(color: Colors.black, size: 50)),
+      textTheme: TextTheme(
+        bodyText2: TextStyle(color: Colors.red),
       ),
-      home: const MyApp()));
+    ),
+    home: const MyApp(),
+  ));
 }
+
 TextStyle a = TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.black);
+
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
   @override
@@ -22,31 +26,50 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   var tab = 0;
+  final PageController _controller = PageController(); // [1]
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("instagram", style: a,),actions: [IconButton(onPressed: (){}, icon: Icon(Icons.add_box_outlined))],),
-      body: [Text('home', style: a,), Text('shopping', style: a,)][tab],
-      bottomNavigationBar: BottomNavigationBar(
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        onTap: (i){
+        title: Text(
+          "instagram",
+          style: a,
+        ),
+        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.add_box_outlined))],
+      ),
+      body: PageView(
+        controller: _controller, // [2]
+        onPageChanged: (i) {
           setState(() {
             tab = i;
+          });
+        },
+        children: [
+          Container(
+            color: Colors.red,
+          ),
+          Container(
+            color: Colors.blue,
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: tab, // [3]
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        onTap: (i) {
+          setState(() {
+            tab = i;
+            _controller.animateToPage(i, // [4]
+                duration: Duration(milliseconds: 3000), curve: Curves.bounceIn);
           });
         },
         items: [
           BottomNavigationBarItem(icon: Icon(Icons.home), label: "home"),
           BottomNavigationBarItem(icon: Icon(Icons.shopping_bag_outlined), label: "shopping"),
         ],
-        // 1. state에 현재 UI의 상태 저장
-        // 2. state에 따라 UI 어떻게 보일지 작성
-        // 3. state를 변경하고 싶을 때 setState() 호출 -> 유저도 state를 변경할 수 있도록
       ),
     );
   }
 }
-
-
